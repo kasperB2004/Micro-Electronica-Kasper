848009b40a52f42b0b2fc8f71c4a5c68987ad536
Kasper Bruneel <kasper.bruneel@leerling.guldensporencollege.be>
2022-07-12 21:24:36 +0200

    started on api stuff

---
+++ EindwerkApi/Controllers/AccountsControler.cs
@@ -0,0 +1,45 @@
+﻿using EindwerkApi.Database;
+using EindwerkApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace EindwerkApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountsControler : Controller
+    {
+        private readonly EindwerkApiDbContext DbContext;
+        public AccountsControler(EindwerkApiDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+
+        [HttpGet(Name ="GetAccountList")]
+        public async Task<IEnumerable<Account>> Get()
+        {
+           return await DbContext.Accounts.ToListAsync();
+        }
+
+
+        [HttpPost(Name = "AddAccount")]
+        public async Task<IActionResult> Post(NewAccountRequest NewAccountRequest)
+        {
+            return BadRequest("UserName Already used");
+
+            Account account = new Account()
+            {
+                Email = NewAccountRequest.Email,
+                FullName = NewAccountRequest.FullName,
+                UserName = NewAccountRequest.UserName,
+                Password = NewAccountRequest.Password
+            };
+            await DbContext.Accounts.AddAsync(account);
+            await DbContext.SaveChangesAsync();
+
+           return Ok(account);
+        }
+    }
+}
---
+++ EindwerkApi/Database/EindwerkApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EindwerkApi.Models;
+
+namespace EindwerkApi.Database
+{
+    public class EindwerkApiDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public EindwerkApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
