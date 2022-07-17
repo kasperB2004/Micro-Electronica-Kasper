558f23268e6941382ffb269e9c781f9e8635c765
Kasper Bruneel <kasper.bruneel@leerling.guldensporencollege.be>
2022-07-17 23:52:29 +0200

    worked on accountsd controller added removing accounts improved new account and add search for account by id or username

--- EindwerkApi/Controllers/AccountsControler.cs
+++ EindwerkApi/Controllers/AccountsControler.cs
@@ -3,12 +3,14 @@ using EindwerkApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
+using EindwerkApi.Encryption;
 
 namespace EindwerkApi.Controllers
 {
     [ApiController]
+    [RequireHttps]
     [Route("api/[controller]")]
-    public class AccountsControler : Controller
+    public class AccountsControler : ControllerBase
     {
         private readonly EindwerkApiDbContext DbContext;
         public AccountsControler(EindwerkApiDbContext dbContext)
@@ -17,29 +19,58 @@ namespace EindwerkApi.Controllers
         }
 
 
-        [HttpGet(Name ="GetAccountList")]
+        [HttpGet("GetAccountList")]
         public async Task<IEnumerable<Account>> Get()
         {
+           //return List of all accounts
            return await DbContext.Accounts.ToListAsync();
+        }
+        [HttpGet("GetAccount/")]
+        public async Task<IActionResult> Get(int? id, string? username)
+        {
+            if (id == null && username == null)
+                return BadRequest();
+            Account? acc = await DbContext.Accounts.AsQueryable().Where(i => i.Id == id || i.UserName == username).FirstOrDefaultAsync();
+            if (acc == null)
+                return NoContent();
+            return Ok(acc);
         }
 
 
-        [HttpPost(Name = "AddAccount")]
+        [HttpPost("AddAccount")]
         public async Task<IActionResult> Post(NewAccountRequest NewAccountRequest)
         {
-            return BadRequest("UserName Already used");
 
+            Account? CheckAccount = await DbContext.Accounts.AsQueryable().Where(i => i.Email == NewAccountRequest.Email || i.UserName == NewAccountRequest.UserName).FirstOrDefaultAsync();
+
+            if(!(CheckAccount == null))
+            {
+                return BadRequest();
+            }
+            
             Account account = new Account()
             {
                 Email = NewAccountRequest.Email,
                 FullName = NewAccountRequest.FullName,
                 UserName = NewAccountRequest.UserName,
-                Password = NewAccountRequest.Password
+                Password = Hashing.hash(Hashing.PasswordGenerator())
             };
             await DbContext.Accounts.AddAsync(account);
             await DbContext.SaveChangesAsync();
-
-           return Ok(account);
+           return Ok();
         }
+
+        [HttpDelete("DeleteAccount")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Account? acc = await DbContext.Accounts.AsQueryable().Where(i =>i.Id == id).FirstOrDefaultAsync();
+            if(acc == null)
+                return NoContent();            
+            DbContext.Accounts.Remove(acc);
+            await DbContext.SaveChangesAsync();
+            return Ok();
+        }   
+
+
     }
 }
---
+++ EindwerkApi/Encryption/Hashing.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Isopoh.Cryptography.Argon2;
+
+namespace EindwerkApi.Encryption
+{
+    public class Hashing
+    {
+        #region SaltGeneration
+        static int DefaultLength = 32;
+        public static byte[] GenerateSalt()
+        {
+            return GenerateSalt(DefaultLength);
+        }
+        public static byte[] GenerateSalt(int Length)
+        {
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            byte[] salt = new byte[Length];
+            rng.GetNonZeroBytes(salt);
+            return salt;
+        }
+        #endregion
+
+        #region Hashing
+        public static string hash(string password)
+        {
+            var config = new Argon2Config
+            {
+                Type = Argon2Type.DataIndependentAddressing,
+                Version = Argon2Version.Nineteen,
+                TimeCost = 10,
+                MemoryCost = 32768,
+                Lanes = 5,
+                Threads = 1, // higher than "Lanes" doesn't help (or hurt)
+                Password = Encoding.UTF8.GetBytes(password),
+                Salt = GenerateSalt(), // >= 8 bytes if not null
+                HashLength = 20 // >= 4
+            };
+            var hash = Argon2.Hash(config);
+            return hash;
+        }
+
+        public static string hash(string password,byte[] Salt)
+        {
+            var config = new Argon2Config
+            {
+                Type = Argon2Type.DataIndependentAddressing,
+                Version = Argon2Version.Nineteen,
+                TimeCost = 10,
+                MemoryCost = 32768,
+                Lanes = 5,
+                Threads = 1, // higher than "Lanes" doesn't help (or hurt)
+                Password = Encoding.UTF8.GetBytes(password),
+                Salt = Salt, // >= 8 bytes if not null
+                HashLength = 20 // >= 4
+            };
+            var hash = Argon2.Hash(config);
+            return hash;
+        }
+        #endregion
+        #region Verify
+        public static bool verify(string hash, string password)
+        {
+            if(Argon2.Verify(hash, password))
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion 
+
+        public static string PasswordGenerator()
+        {
+            IEnumerable<char> characterSet =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+        "abcdefghijklmnopqrstuvwxyz" +
+        "0123456789" +
+        "*$-+?_&=!%{}/";
+            var characterArray = characterSet.Distinct().ToArray();
+            var bytes = new byte[8 * 8];
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            rng.GetNonZeroBytes(bytes);
+            var result = new char[8];
+            for (int i = 0; i < 8; i++)
+            {
+                ulong value = BitConverter.ToUInt64(bytes, i * 8);
+                result[i] = characterArray[value % (uint)characterArray.Length];
+            }
+            return new string(result);
+        }
+    }
+}
